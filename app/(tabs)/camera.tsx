4a263e427bc6da4d9cf99a9204190189943bca15
Svilen Andonov <svilenbs@gmail.com>
2024-08-07 17:37:03 +0300

    Add camera dummy logic

---
+++ app/(tabs)/camera.tsx
@@ -0,0 +1,91 @@
+import { CameraView, CameraType, useCameraPermissions, Camera } from 'expo-camera';
+import { useState, useRef } from 'react';
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+export default function App() {
+  const [facing, setFacing] = useState<CameraType>('back');
+  const [permission, requestPermission] = useCameraPermissions();
+  const cameraRef = useRef(null);
+  if (!permission) {
+    // Camera permissions are still loading.
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    // Camera permissions are not granted yet.
+    return (
+      <View style={styles.container}>
+        <Text style={styles.message}>We need your permission to show the camera</Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    );
+  }
+
+  function toggleCameraFacing() {
+    setFacing(current => (current === 'back' ? 'front' : 'back'));
+  }
+
+  function takePicture() {
+      if (cameraRef.current) {
+        const start = Date.now();
+        
+        cameraRef.current
+          ?.takePictureAsync({
+            skipProcessing: true,
+            base64: true
+          })
+          .then((photoData) => {
+   
+            console.log(`Delay after takePictureAsync: ${Date.now()- start} ms`);
+            console.log('base 64 is ' + photoData.base64)
+            setImageUri(photoData.uri);
+          });
+      }
+    };
+   
+
+  return (
+    <View style={styles.container}>
+      <CameraView style={styles.camera} facing={facing}  ref={cameraRef}>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
+            <Text style={styles.text}>Flip Camera</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={takePicture}>
+            <Text style={styles.text}>Take picture</Text>
+          </TouchableOpacity>
+        </View>
+      </CameraView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  message: {
+    textAlign: 'center',
+    paddingBottom: 10,
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    margin: 64,
+  },
+  button: {
+    flex: 1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+});
